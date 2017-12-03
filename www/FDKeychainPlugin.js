c39f3e16f06b7bfcc11ef6c80c13cea0ef5e987d
Marcos <marcos0rodriguez0diaz@gmail.com>
2017-12-03 13:44:11 +0100

    Some corrections

--- www/FDKeychainPlugin.js
+++ www/FDKeychainPlugin.js
@@ -3,26 +3,26 @@ var exec = require('cordova/exec');
 var FDKeychain = {
 	serviceName: "FDKeychainPlugin",
 
-	getItemForService: function(success, error, key, forService) {
-		exec(success, error, this.serviceName, "get", [key, forService, inAccessGroup]);
+	getItemForService: function(successCallback, failureCallback, key, forService) {
+		exec(successCallback, failureCallback, this.serviceName, "get", [key, forService]);
 	},
-	getItemForServiceInAccessGroup: function(success, error, key, forService, inAccessGroup) {
-		exec(success, error, this.serviceName, "get", [key, forService, inAccessGroup]);
+	getItemForServiceInAccessGroup: function(successCallback, failureCallback, key, forService, inAccessGroup) {
+		exec(successCallback, failureCallback, this.serviceName, "get", [key, forService, inAccessGroup]);
 	},
-	getRawDataForService: function(success, error, key, forService) {
-		exec(success, error, this.serviceName, "getRaw", [key, forService, inAccessGroup]);
+	getRawDataForService: function(successCallback, failureCallback, key, forService) {
+		exec(successCallback, failureCallback, this.serviceName, "getRaw", [key, forService]);
 	},
-	getRawDataForServiceInAccessGroup: function(success, error, key, forService, inAccessGroup) {
-		exec(success, error, this.serviceName, "getRaw", [key, forService, inAccessGroup]);
+	getRawDataForServiceInAccessGroup: function(successCallback, failureCallback, key, forService, inAccessGroup) {
+		exec(successCallback, failureCallback, this.serviceName, "getRaw", [key, forService, inAccessGroup]);
 	},
-	setItemForService: function(success, error, key, value, forService, onlyAccessibleWhenUnlocked) {
-		exec(success, error, this.serviceName, "set", [key, value, forService, inAccessGroup, onlyAccessibleWhenUnlocked]);
+	setItemForService: function(successCallback, failureCallback, key, value, forService, onlyAccessibleWhenUnlocked) {
+		exec(successCallback, failureCallback, this.serviceName, "set", [key, value, forService, onlyAccessibleWhenUnlocked]);
 	},
-	setItemForServiceInAccessGroup: function(success, error, key, value, forService, inAccessGroup, onlyAccessibleWhenUnlocked) {
-		exec(success, error, this.serviceName, "set", [key, value, forService, inAccessGroup, onlyAccessibleWhenUnlocked]);
+	setItemForServiceInAccessGroup: function(successCallback, failureCallback, key, value, forService, inAccessGroup, onlyAccessibleWhenUnlocked) {
+		exec(successCallback, failureCallback, this.serviceName, "set", [key, value, forService, inAccessGroup, onlyAccessibleWhenUnlocked]);
 	},
 	removeItemForService: function(successCallback, failureCallback, key, forService) {
-		exec(successCallback, failureCallback, this.serviceName, "remove", [key, forService, inAccessGroup]);
+		exec(successCallback, failureCallback, this.serviceName, "remove", [key, forService]);
 	},
 	removeItemForServiceInAccessGroup: function(successCallback, failureCallback, key, forService, inAccessGroup) {
 		exec(successCallback, failureCallback, this.serviceName, "remove", [key, forService, inAccessGroup]);
